8dc2732f50a9b8820621fa7aac17dfb6a4af0561
윤준하 <jhy@colorverse.co>
2023-06-27 13:46:00 +0900

    feat: export car function to .babylon

--- src/pages/Tutorial/Car/index.tsx
+++ src/pages/Tutorial/Car/index.tsx
@@ -8,7 +8,8 @@ import {
   ArcRotateCamera,
   StandardMaterial,
   Texture,
-  Animation
+  Animation,
+  SceneLoader
 } from '@babylonjs/core';
 import earcut from 'earcut';
 
@@ -37,96 +38,107 @@ const onSceneReady = (scene: Scene) => {
   const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene);
   light.intensity = 0.8;
 
-  const car = buildCar(scene);
+
+  SceneLoader.ImportMeshAsync('', '/src/assets/ex_tutorial/', 'car.babylon').then(() => {
+    const wheelRF = scene.getMeshByName('wheelRF');
+    const wheelRB = scene.getMeshByName('wheelRB');
+    const wheelLF = scene.getMeshByName('wheelLF');
+    const wheelLB = scene.getMeshByName('wheelLB');
+    console.log(wheelRB?.animations);
+    scene.beginAnimation(wheelRF, 0, 30, true);
+    scene.beginAnimation(wheelRB, 0, 30, true);
+    scene.beginAnimation(wheelLF, 0, 30, true);
+    scene.beginAnimation(wheelLB, 0, 30, true);
+  });
+
+  // buildCar(scene);
 
   return scene;
 }
 
-const buildCar = (scene: Scene) => {
-  //base
-  const outline = [
-    new Vector3(-0.3, 0, -0.1),
-    new Vector3(0.2, 0, -0.1)
-  ]
-  // curved front
-  for (let i = 0; i < 20; i++) {
-    outline.push(new Vector3(0.2 * Math.cos(i * Math.PI / 40), 0, 0.2 * Math.sin(i * Math.PI / 40) - 0.1));
-  }
+/** car.babylon 파일에서 수행하는 내용 */
+// const buildCar = (scene: Scene) => {
+//   //base
+//   const outline = [
+//     new Vector3(-0.3, 0, -0.1),
+//     new Vector3(0.2, 0, -0.1)
+//   ]
+//   // curved front
+//   for (let i = 0; i < 20; i++) {
+//     outline.push(new Vector3(0.2 * Math.cos(i * Math.PI / 40), 0, 0.2 * Math.sin(i * Math.PI / 40) - 0.1));
+//   }
 
-  // top
-  outline.push(new Vector3(0, 0, 0.1));
-  outline.push(new Vector3(-0.3, 0, 0.1));
+//   // top
+//   outline.push(new Vector3(0, 0, 0.1));
+//   outline.push(new Vector3(-0.3, 0, 0.1));
 
-  const faceUV = [];
-  faceUV[0] = new Vector4(0, 0.5, 0.38, 1);
-  faceUV[1] = new Vector4(0, 0, 1, 0.5);
-  faceUV[2] = new Vector4(0.38, 1, 0, 0.5);
-  const carMat = new StandardMaterial('carMat');
-  carMat.diffuseTexture = new Texture('https://assets.babylonjs.com/environments/car.png');
+//   const faceUV = [];
+//   faceUV[0] = new Vector4(0, 0.5, 0.38, 1);
+//   faceUV[1] = new Vector4(0, 0, 1, 0.5);
+//   faceUV[2] = new Vector4(0.38, 1, 0, 0.5);
+//   const carMat = new StandardMaterial('carMat');
+//   carMat.diffuseTexture = new Texture('https://assets.babylonjs.com/environments/car.png');
 
-  //back formed automatically
-  const car = MeshBuilder.ExtrudePolygon('car', { shape: outline, depth: 0.2, faceUV: faceUV, wrap: true }, scene, earcut);
-  car.material = carMat;
+//   //back formed automatically
+//   const car = MeshBuilder.ExtrudePolygon('car', { shape: outline, depth: 0.2, faceUV: faceUV, wrap: true }, scene, earcut);
+//   car.material = carMat;
 
-  const wheelUV = [];
-  wheelUV[0] = new Vector4(0, 0, 1, 1);
-  wheelUV[1] = new Vector4(0, 0.5, 0, 0.5);
-  wheelUV[2] = new Vector4(0, 0, 1, 1);
+//   const wheelUV = [];
+//   wheelUV[0] = new Vector4(0, 0, 1, 1);
+//   wheelUV[1] = new Vector4(0, 0.5, 0, 0.5);
+//   wheelUV[2] = new Vector4(0, 0, 1, 1);
 
-  const wheelMat = new StandardMaterial('wheelMat');
-  wheelMat.diffuseTexture = new Texture('https://assets.babylonjs.com/environments/wheel.png');
+//   const wheelMat = new StandardMaterial('wheelMat');
+//   wheelMat.diffuseTexture = new Texture('https://assets.babylonjs.com/environments/wheel.png');
 
-  const wheelRB = MeshBuilder.CreateCylinder('wheelRB', { diameter: 0.125, height: 0.05, faceUV: wheelUV });
-  wheelRB.material = wheelMat;
-  wheelRB.parent = car;
-  wheelRB.position = new Vector3(-0.2, 0.035, -0.1);
+//   const wheelRB = MeshBuilder.CreateCylinder('wheelRB', { diameter: 0.125, height: 0.05, faceUV: wheelUV });
+//   wheelRB.material = wheelMat;
+//   wheelRB.parent = car;
+//   wheelRB.position = new Vector3(-0.2, 0.035, -0.1);
 
-  const wheelRF = wheelRB.clone('wheelRF');
-  wheelRF.position.x = 0.1;
-  const wheelLB = wheelRB.clone('wheelLB');
-  wheelLB.position.y = -0.2 - 0.035;
-  // const wheelLF = wheelRB.clone('wheelLF');
-  // wheelLF.position.x = 0.1;
-  // wheelLF.position.y = -0.2 - 0.035;
-  const wheelLF = wheelRF.clone('wheelLF');
-  wheelLF.position.y = -0.2 - 0.035;
+//   const wheelRF = wheelRB.clone('wheelRF');
+//   wheelRF.position.x = 0.1;
+//   const wheelLB = wheelRB.clone('wheelLB');
+//   wheelLB.position.y = -0.2 - 0.035;
+//   // const wheelLF = wheelRB.clone('wheelLF');
+//   // wheelLF.position.x = 0.1;
+//   // wheelLF.position.y = -0.2 - 0.035;
+//   const wheelLF = wheelRF.clone('wheelLF');
+//   wheelLF.position.y = -0.2 - 0.035;
 
-  // Animate wheels
-  const animWheel = new Animation('wheelAnimation', 'rotation.y', 30, Animation.ANIMATIONTYPE_FLOAT, Animation.ANIMATIONLOOPMODE_CYCLE);
+//   // Animate wheels
+//   const animWheel = new Animation('wheelAnimation', 'rotation.y', 30, Animation.ANIMATIONTYPE_FLOAT, Animation.ANIMATIONLOOPMODE_CYCLE);
 
-  const wheelKeys = [];
+//   const wheelKeys = [];
 
-  // At the animation key 0, the value of roation.y is 0
-  wheelKeys.push({
-    frame: 0,
-    value: 0
-  });
+//   // At the animation key 0, the value of roation.y is 0
+//   wheelKeys.push({
+//     frame: 0,
+//     value: 0
+//   });
 
-  // At the animation key 30, (after 1 sec since animation fps = 30) the value of rotation.y is 2PI for a complete rotation
-  wheelKeys.push({
-    frame: 30,
-    value: 2 * Math.PI
-  })
+//   // At the animation key 30, (after 1 sec since animation fps = 30) the value of rotation.y is 2PI for a complete rotation
+//   wheelKeys.push({
+//     frame: 30,
+//     value: 2 * Math.PI
+//   })
 
-  // Set the keys
-  animWheel.setKeys(wheelKeys);
+//   // Set the keys
+//   animWheel.setKeys(wheelKeys);
 
-  // Link this animation th a wheel
-  wheelRB.animations = [];
-  wheelRB.animations.push(animWheel);
-  wheelRF.animations.push(animWheel);
-  wheelLB.animations.push(animWheel);
-  wheelLF.animations.push(animWheel);
+//   // Link this animation th a wheel
+//   wheelRB.animations = [];
+//   wheelRB.animations.push(animWheel);
+//   wheelRF.animations.push(animWheel);
+//   wheelLB.animations.push(animWheel);
+//   wheelLF.animations.push(animWheel);
 
-  scene.beginAnimation(wheelRB, 0, 30, true);
-  scene.beginAnimation(wheelRF, 0, 30, true);
-  scene.beginAnimation(wheelLB, 0, 30, true);
-  scene.beginAnimation(wheelLF, 0, 30, true);
+//   scene.beginAnimation(wheelRB, 0, 30, true);
+//   scene.beginAnimation(wheelRF, 0, 30, true);
+//   scene.beginAnimation(wheelLB, 0, 30, true);
+//   scene.beginAnimation(wheelLF, 0, 30, true);
 
-
-
-  return car;
-}
+// }
 
 
 const Car = () => {
